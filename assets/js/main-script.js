d4b73323f284db3e43fb93dc1a2f71874041695c
andron13 <andron13@gmail.com>
2021-09-30 15:40:37 +0200

    feat: max-width img 100vw

---
+++ assets/js/main-script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let galleryBarcelona = document.getElementsByClassName("barcelona");
+let pathGallery = "assets/img/gallery";
+let lastPhoto = 25; // for barcelona gallery last photo 25
+
+function fillImage(){
+
+}
